03dcc1c1ef9cb99cbce8a9458fddab2bf571f5d5
irishfarma <lukapavageau44@gmail.com>
2024-03-20 09:44:11 +0100

    modular code

---
+++ python/backend/clean_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy
+
+def replace_all(df):
+        for column in df.columns:
+                df[column] = pd.to_numeric(df[column], errors='coerce') # Dans toutes les colones on converti chaque valeur en float, si erreur remplacer nan
+
+        df.fillna(0, inplace=True) # remplace nan par 0 partout
+        return df
+
+
+def select_paired_lines(df):
+
+        dfcotes = df[df['N° Cote'] > 10] # ne conserve que les lignes ou la valeur de la colonne est supérieure à 10 pour éliminer tous les espaces 
+        indices = dfcotes.index.tolist()
+        indices_to_keep = list(indices)  # Utilisation d'un set pour éviter les doublons
+
+                # Ajout de l'indice de la ligne suivante pour chaque ligne conforme
+        for index in indices:
+                next_index = index + 1
+                if next_index in df.index:  # Assurez-vous que l'indice suivant est dans le DataFrame original
+                                indices_to_keep.append(next_index)
+
+                # Création du nouveau DataFrame avec les lignes sélectionnées
+        dcotes = df.loc[indices_to_keep]
+
+                # Tri du DataFrame selon l'ordre initial des indices
+        dc = dcotes.sort_index()
+        dc.reset_index(drop=True, inplace=True) 
+        return dc
+
+def clean_data(df):
+        df = replace_all(df)
+        dc = select_paired_lines(df)
+        return dc
---
+++ python/backend/result.py
@@ -0,0 +1,34 @@
+
+
+def check_row(row, nominal, lim_inf, lim_sup):
+    return all([
+        (row[f'Mesure{i}'] == 0) or (row[f'Mesure{i}'] < (nominal + lim_sup) and row[f'Mesure{i}'] > (nominal + lim_inf)) 
+        for i in range(1, 6)
+    ])
+
+def filter_conformity(dc):
+# Création d'une liste pour les indices des lignes à conserver
+    indices_to_keep = []
+    indice_to_move = []
+    # Parcours du DataFrame en sautant une ligne à chaque fois (pour traiter les paires)
+    for index in range(0, len(dc), 2):
+        if index + 1 in dc.index:  # Vérifier que la paire existe
+            row1 = dc.loc[index]
+            row2 = dc.loc[index + 1]
+            nominal, lim_inf, lim_sup = row1['Valeur nominale'], row1['Limite Inf'], row1['Limite Sup']
+
+            # Conserver la paire si les deux lignes répondent aux critères
+            if check_row(row1, nominal, lim_inf, lim_sup) and check_row(row2, nominal, lim_inf, lim_sup):
+                indices_to_keep.extend([index, index + 1])
+            else:
+                indice_to_move.extend([index, index + 1])
+
+
+
+
+    dfConf = dc.loc[indices_to_keep] # conformes
+    dfNConf= dc.loc[indice_to_move] # non conformes
+        # Affiche toutes les côtes conformes
+    conformes = len(dfConf)/2
+    nonConformes = len(dfNConf)/2
+    return dfConf, dfNConf, conformes, nonConformes
